d705fe626142d6cc75d769290af808bed62700c3
Shun E <shun-eto@sopherre.com>
2022-02-07 17:18:49 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('NestJS Tutorial')
+    .setDescription('The NestJS Tutorial API description')
+    .setVersion('1.0')
+    .addTag('cats', 'description')
+    .addTag('users', 'description')
+    .addTag('(root)')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
